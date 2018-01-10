456f30efd7fbcc475612df2181782cadf134e2fe
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-09 22:20:39 -0500

    Mumuki Export on 2018-01-09 22:20:39 -0500

--- 00009_Al menos tenemos (menos) salud/test.rb
+++ 00009_Al menos tenemos (menos) salud/test.rb
@@ -1,1 +1,8 @@
-#I will do it
+it "La clase Zombie entiende el mensaje initialize" do
+  expect(Zombie.initialize(3)). not to raise_error
+end
+
+it "Si creo un Zombie con salud 50 su salud es 50" do
+  zombie = Zombie.new(50)
+  expect(zombie.salud). to be 50
+end
