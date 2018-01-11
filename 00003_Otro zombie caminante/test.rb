70bf18947755979b6c146fa3459e234ad426a87f
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-10 21:57:41 -0500

    Mumuki Export on 2018-01-10 21:57:41 -0500

--- 00003_Otro zombie caminante/test.rb
+++ 00003_Otro zombie caminante/test.rb
@@ -23,21 +23,21 @@ it "Kiki tiene 100 de salud inicialmente" do
 end
 
 it "Si Bouba recibe 5 puntos de daño disminuye su salud en 10 puntos" do
-  Bouba.recibir_daño(5)
+  Bouba.recibir_danio(5)
   expect(Bouba.salud).to eq 90
 end
 
 it "Si Kiki recibe 5 puntos de daño disminuye su salud en 10 puntos" do
-  Kiki.recibir_daño(5)
+  Kiki.recibir_danio(5)
   expect(Kiki.salud).to eq 90
 end
 
 it "Si Bouba recibe mucho daño su salud no puede ser menor a 0" do
-  Bouba.recibir_daño(69)
+  Bouba.recibir_danio(69)
   expect(Bouba.salud).to eq 0
 end
 
 it "Si Kiki recibe mucho daño su salud no puede ser menor a 0" do
-  Kiki.recibir_daño(69)
+  Kiki.recibir_danio(69)
   expect(Kiki.salud).to eq 0
 end
