d1104d6780480767cdbe77f510c36d8c7a27f1ac
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-10 22:08:37 -0500

    Mumuki Export on 2018-01-10 22:08:37 -0500

--- 00004_¡¿Vivos?!/test.rb
+++ 00004_¡¿Vivos?!/test.rb
@@ -25,25 +25,25 @@ it "Kiki tiene 100 de salud inicialmente y está con vida" do
 end
 
 it "Si Bouba recibe 5 puntos de daño disminuye su salud en 10 puntos y está con vida" do
-  Bouba.recibir_daño(5)
+  Bouba.recibir_danio!(5)
   expect(Bouba.salud).to eq 90
   expect(Bouba.sin_vida?).to eq false
 end
 
 it "Si Kiki recibe 5 puntos de daño disminuye su salud en 10 puntos y está con vida" do
-  Kiki.recibir_daño(5)
+  Kiki.recibir_danio!(5)
   expect(Kiki.salud).to eq 90
   expect(Kiki.sin_vida?).to eq false
 end
 
 it "Si Bouba recibe mucho daño su salud es 0 y no está con vida" do
-  Bouba.recibir_daño(69)
+  Bouba.recibir_danio!(69)
   expect(Bouba.salud).to eq 0
   expect(Bouba.sin_vida?).to eq true
 end
 
 it "Si Kiki recibe mucho daño su salud es 0 y no está con vida" do
-  Kiki.recibir_daño(69)
+  Kiki.recibir_danio!(69)
   expect(Kiki.salud).to eq 0
   expect(Kiki.sin_vida?).to eq true
 end
