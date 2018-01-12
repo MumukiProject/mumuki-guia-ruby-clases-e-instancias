33bbf17bf420b33f7fe4ba044f7ddaa39bb72232
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-12 13:40:35 -0500

    Mumuki Export on 2018-01-12 13:40:35 -0500

--- 00005_Clases/test.rb
+++ 00005_Clases/test.rb
@@ -12,13 +12,13 @@ it "Un Zombie tiene 100 de salud inicialmente y está con vida" do
 end
 
 it "Si un Zombie recibe 5 puntos de daño disminuye su salud en 10 puntos y está con vida" do
-  Zombie.recibir_daño(5)
+  Zombie.recibir_danio!(5)
   expect(Zombie.salud).to eq 90
   expect(Zombie.sin_vida?).to eq false
 end
 
 it "Si un Zombie recibe mucho daño su salud es 0 y no está con vida" do
-  Zombie.recibir_daño(69)
+  Zombie.recibir_danio!(69)
   expect(Zombie.salud).to eq 0
   expect(Zombie.sin_vida?).to eq true
 end
