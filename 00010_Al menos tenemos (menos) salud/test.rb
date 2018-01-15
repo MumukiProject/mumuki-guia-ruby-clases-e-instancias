51c46715e4caf4d24a1c327b43889838da5f863d
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-15 00:05:54 -0500

    Mumuki Export on 2018-01-15 00:05:54 -0500

--- 00010_Al menos tenemos (menos) salud/test.rb
+++ 00010_Al menos tenemos (menos) salud/test.rb
@@ -1,33 +1,30 @@
-it "La clase Zombi entiende el mensaje initialize" do
-  expect(Zombi.initialize(3)). not to raise_error
-end
+zombi = Zombi.new(100)
 
 it "Si creo un Zombi con salud 50 su salud es 50" do
-  zombi = Zombi.new(50)
-  expect(zombi.salud). to be 50
+  zombi50 = Zombi.new(50)
+  expect(zombi50.salud). to be 50
 end
 
 it "Un Zombi no sabe correr" do
-  expect(Zombi.sabe_correr?).to be false
+  expect(zombi.sabe_correr?).to be false
 end
 
 it "Un Zombi grita ¡agrrrg!" do
-  expect(Zombi.gritar).to eq "¡agrrrg!"
+  expect(zombi.gritar).to eq "¡agrrrg!"
 end
 
-it "Un Zombi tiene 100 de salud inicialmente y está con vida" do
-  expect(Zombi.salud).to eq 100
-  expect(Zombi.sin_vida?).to eq false
+it "Un Zombi con salud inicial 100 no está sin vida" do
+  expect(zombi.sin_vida?).to eq false
 end
 
-it "Si un Zombi recibe 5 puntos de daño disminuye su salud en 10 puntos y está con vida" do
-  Zombi.recibir_danio!(5)
-  expect(Zombi.salud).to eq 90
-  expect(Zombi.sin_vida?).to eq false
+it "Si un Zombi de salud 100 recibe 5 puntos de daño disminuye su salud en 10 puntos y no está sin vida" do
+  zombi.recibir_danio!(5)
+  expect(zombi.salud).to eq 90
+  expect(zombi.sin_vida?).to eq false
 end
 
-it "Si un Zombi recibe mucho daño su salud es 0 y no está con vida" do
-  Zombi.recibir_danio!(69)
-  expect(Zombi.salud).to eq 0
-  expect(Zombi.sin_vida?).to eq true
+it "Si un Zombi recibe mucho daño su salud es 0 y está sin vida" do
+  zombi.recibir_danio!(69)
+  expect(zombi.salud).to eq 0
+  expect(zombi.sin_vida?).to eq true
 end
