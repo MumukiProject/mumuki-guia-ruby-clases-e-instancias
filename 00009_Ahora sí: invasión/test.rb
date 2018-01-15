e3806baa5be8300b826f7c5271dfe90fa6a3caa1
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-01-14 23:54:34 -0500

    Mumuki Export on 2018-01-14 23:54:34 -0500

--- 00009_Ahora sí: invasión/test.rb
+++ 00009_Ahora sí: invasión/test.rb
@@ -24,12 +24,34 @@ class Zombi
   end
 end
 
+bouba = Zombi.new
+kiki = Zombi.new
+#Zombi, bouba y kiki van aquí para no ensuciar la Biblioteca, que muestra el código de Juliana.
+
 it "caminantes tiene veinte elementos" do
   expect(caminantes.size). to eq 20
 end
 
 it "Todos los caminantes son zombies" do
   expect(caminantes.all? { |caminante| caminante.instance_of? Zombi } ). to be true
+end
+
+it "La energía inicial de juliana es 1000" do
+  expect(juliana.energia).to eq 1000
+end
+
+it "La energía inicial de anastasia es 1000" do
+  expect(anastasia.energia).to eq 1000
+end
+
+it "Si juliana ataca con 5 puntos de daño a un zombi su salud disminuye en 10 puntos" do
+  juliana.atacar!(bouba, 5)
+  expect(bouba.salud).to eq 90
+end
+
+it "Si anastasia ataca con 5 puntos de daño a un zombi su salud disminuye en 10 puntos" do
+  anastasia.atacar!(kiki, 5)
+  expect(kiki.salud).to eq 90
 end
 
 it "Cuando juliana ataca masivamente les reduce la vida a todos los zombis en 30 puntos" do
